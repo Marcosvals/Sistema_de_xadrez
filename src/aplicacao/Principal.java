169888033602cdf86b8af249349be6e7bd628912
Marcos Vinicius <marcosgaba7@gmail.com>
2021-12-28 03:36:38 -0300

    Classe posicionamento do pacote tabuleiro criada

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,5 +1,11 @@
 package aplicacao;
 
-public class Principal {
+import tabuleiro.Posicionamento;
 
+public class Principal {
+	public static void main(String[] args) {
+		Posicionamento posicao = new Posicionamento(1,2);
+		System.out.println(posicao);
+		
+	}
 }
