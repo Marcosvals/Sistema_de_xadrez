ade8438c90f63c2542a0dbb008c476fe2e61dc69
Marcos Vinicius <marcosgaba7@gmail.com>
2022-01-26 11:05:09 -0300

    Rainha

---
+++ src/xadrez/pecas/Rainha.java
@@ -0,0 +1,107 @@
+package xadrez.pecas;
+
+import tabuleiro.Posicionamento;
+import tabuleiro.Tabuleiro;
+import xadrez.Color;
+import xadrez.PecaDeXadrez;
+
+public class Rainha extends PecaDeXadrez{
+	
+	public Rainha(Tabuleiro tabuleiro, Color color) {
+		super(tabuleiro, color);
+	}
+
+	@Override
+	public String toString() {
+		return "D";
+	}
+	
+	@Override
+	public boolean[][] possivelMovimento() {
+		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		
+		Posicionamento p = new Posicionamento(0, 0);
+		
+		//above
+		p.setValues(posicionamento.getLinha() - 1, posicionamento.getColuna());
+		while(getTabuleiro().posicionamentoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha()-1);
+		}
+		if(getTabuleiro().posicionamentoExiste(p) && ExistePecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//left
+		p.setValues(posicionamento.getLinha(), posicionamento.getColuna() - 1);
+		while(getTabuleiro().posicionamentoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() - 1);
+		}
+		if(getTabuleiro().posicionamentoExiste(p) && ExistePecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//right
+		p.setValues(posicionamento.getLinha(), posicionamento.getColuna() + 1);
+		while(getTabuleiro().posicionamentoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() + 1);
+		}
+		if(getTabuleiro().posicionamentoExiste(p) && ExistePecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//below
+		p.setValues(posicionamento.getLinha() + 1, posicionamento.getColuna());
+		while(getTabuleiro().posicionamentoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() + 1);
+		}
+		if(getTabuleiro().posicionamentoExiste(p) && ExistePecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//noroeste
+		p.setValues(posicionamento.getLinha() - 1, posicionamento.getColuna() - 1);
+		while(getTabuleiro().posicionamentoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() - 1, p.getColuna() - 1);
+		}
+		if(getTabuleiro().posicionamentoExiste(p) && ExistePecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//nordeste
+		p.setValues(posicionamento.getLinha() - 1, posicionamento.getColuna() + 1);
+		while(getTabuleiro().posicionamentoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if(getTabuleiro().posicionamentoExiste(p) && ExistePecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//sudeste
+		p.setValues(posicionamento.getLinha() + 1, posicionamento.getColuna() + 1);
+		while(getTabuleiro().posicionamentoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() + 1 , p.getColuna() + 1);
+		}
+		if(getTabuleiro().posicionamentoExiste(p) && ExistePecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//sudoeste
+		p.setValues(posicionamento.getLinha() + 1, posicionamento.getColuna() - 1);
+		while(getTabuleiro().posicionamentoExiste(p) && !getTabuleiro().TemUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if(getTabuleiro().posicionamentoExiste(p) && ExistePecaAdversaria(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		return mat;
+	}
+}
